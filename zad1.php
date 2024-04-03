abc35773e5e700a737c0e5be825770dbe78903f1
s28413-pj <117545944+28413-pj@users.noreply.github.com>
2024-04-03 20:11:16 +0200

    Add files via upload

---
+++ zad1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$owoce = array("jablko", "banan", "pomarancza");
+
+foreach ($owoce as $owoc) {
+    $dlugosc = strlen($owoc);
+    echo "Owoc: ";
+    for ($i = $dlugosc - 1; $i >= 0; $i--) {
+        echo $owoc[$i];
+    }
+    echo "<br>";
+
+    
+    if ($owoc[0] == 'p' || $owoc[0] == 'P') {
+        echo "Rozpoczyna się od litery 'p'.";
+    } else {
+        echo "Nie rozpoczyna się od litery 'p'.";
+    }
+    echo "<br><br>";
+}
+?>
+
+</body>
+</html>
